b5ab34a7081e76d11cf20c284ab0262b0622bd1c
Njunge Chuguka <105584407+kevnjunge@users.noreply.github.com>
2023-03-14 23:23:10 +0300

    get handlers test

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alexedwards/scs/v2 v2.5.0
 	github.com/go-chi/chi v1.5.4
 	github.com/justinas/nosurf v1.1.1
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 
 )
 
-require github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+
---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,50 @@
+package handlers
+
+
+
+type postData struct {
+	key string
+	value string
+}
+
+var theTests []struct{
+	name string
+	url string
+	method string
+	params []postData
+    expectedStatusCode int
+}{
+	{"home", "/", "GET",[]postData{}, http.StatusOk},
+	{"about", "/about", "GET",[]postData{}, http.StatusOk},
+	{"gq", "/generals-quarters", "GET",[]postData{}, http.StatusOk},
+	{"ms", "/majors-suite", "GET",[]postData{}, http.StatusOk},
+	{"sa", "/search-availability", "GET",[]postDataa{}, http.StatusOk},
+	{"contact", "/contact", "GET",[]postDataa{}, http.StatusOk},
+	
+
+
+}
+
+func TestHandlers(t *testing.T){
+	routes := getRoutes()
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range theTests{
+		if e.method == "GET"{
+			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil{
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode{
+				t.Errorf("for %s, expected %d but got %d",e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+
+		}else{
+
+		}
+	}
+}
+
